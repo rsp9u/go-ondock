c9a412c67909c77112ce7a8e56823bbae40fb59b
rsp9u <gctwt852@yahoo.co.jp>
2019-04-29 10:47:41 +0900

    Change the mount path from current-directory to GOPATH-like directory

--- go-ondock.sh
+++ go-ondock.sh
@@ -1,16 +1,34 @@
 #!/bin/sh
 GOVERSION="REPLACE_GOVERSION"
 NAMEBASE="go-ondock"
-NAME="${NAMEBASE}$(pwd | sed -e 's,/,-,g')"
+NAME="${NAMEBASE}-$(whoami)"
+
+search_host_gopath() {
+  if [ -d $HOME/go ]; then
+    echo "$HOME/go"
+  elif [ -d /go ]; then
+    echo "/go"
+  fi
+}
 
 if [ $(docker ps --filter=name=${NAME} | wc -l) -lt 2 ]; then
   if [ $(docker ps --format='{{.Names}}' | grep ${NAMEBASE} | wc -l) -gt 5 ]; then
-	oldest=$(docker ps --format='{{.ID}}' | grep ${NAMEBASE} | tail -n1)
-	docker rm -f ${oldest} > /dev/null 2>&1
+    oldest=$(docker ps --format='{{.ID}}' | grep ${NAMEBASE} | tail -n1)
+    docker rm -f ${oldest} > /dev/null 2>&1
   fi
-  docker run -tid --rm --name ${NAME} -v /tmp:/tmp -v $PWD:$PWD local/golang:${GOVERSION}-alpine /bin/ash > /dev/null 2>&1
+  host_gopath=$(search_host_gopath)
+  if [ "${host_gopath}" = "" ]; then
+    echo "Not found anything that looks like GOPATH on the host."
+    echo "You must make the directory located '~/go' or '/go'."
+    exit 1
+  fi
+  docker run -tid --rm --name ${NAME} -v /tmp:/tmp -v ${host_gopath}/src:/go/src local/golang:${GOVERSION}-alpine /bin/ash > /dev/null 2>&1
 fi
 user=$(id -u):$(id -g)
 goenv=$(env | grep "^GO")
-cmd="cd $PWD && $(basename $0) $@"
-docker exec -i -u ${user} -e ${goenv} ${NAME} /bin/ash -c "${cmd}"
+cmd="cd $(pwd | sed -e "s,$HOME,,g") && $(basename $0) $@"
+if [ "${goenv}" = "" ];then
+  docker exec -i -u ${user} ${NAME} /bin/ash -c "${cmd}"
+else
+  docker exec -i -u ${user} -e ${goenv} ${NAME} /bin/ash -c "${cmd}"
+fi
